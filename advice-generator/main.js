99934851b4ff75b8f31046bfdee5b578148e2656
SamBeovides <abasbeovides@gmail.com>
2023-02-10 16:00:20 -0500

    landing page design update

--- advice-generator/main.js
+++ advice-generator/main.js
@@ -1,14 +1,17 @@
 const dicebutton=document.querySelector(".dice-container");
 const adviceid=document.querySelector(".advice-id");
 const advicetext=document.querySelector(".advice-text");
+const apiurl="https://api.adviceslip.com/advice";
 
 dicebutton.addEventListener("click",() => {
-    fetch("https://api.adviceslip.com/advice")
+    fetch(apiurl,{cache: "no-cache"})
     .then(res=>res.json())
-    .then(data=>{
-        let newid=data.slip.id;
-        let newadvice=data.slip.advice; 
-        adviceid.firstChild.innerHTML="ADVICE #"+newid;
+    .then(res=>{
+        let data=res.slip;
+        let newid=data.id;
+        let newadvice=data.advice; 
+
+        adviceid.firstChild.innerHTML=`ADVICE # ${newid}`;
         advicetext.firstChild.innerHTML=newadvice;
     })
 });
